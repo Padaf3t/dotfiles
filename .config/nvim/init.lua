69eb1197bd0ae9158183c44a2f27198c9d2aa4b1
Padaf3t <contact@facarrier.com>
2025-06-06 07:54:21 -0400

    begin working on config of wofi

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,3 +2,15 @@
 require("config.lazy")
 vim.o.background = "light"
 vim.cmd([[colorscheme gruvbox]])
+vim.g.clipboard = {
+  name = "wl-clipboard",
+  copy = {
+    ["+"] = "wl-copy --foreground --type text/plain",
+    ["*"] = "wl-copy --foreground --primary --type text/plain",
+  },
+  paste = {
+    ["+"] = "wl-paste --no-newline",
+    ["*"] = "wl-paste --no-newline --primary",
+  },
+  cache_enabled = 1,
+}
