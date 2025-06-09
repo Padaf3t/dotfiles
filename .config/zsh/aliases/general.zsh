d52c255b7b87549b6126395fd182358086a96be5
Padaf3t <contact@facarrier.com>
2025-06-08 21:06:00 -0400

    Add alias

--- .config/zsh/aliases/general.zsh
+++ .config/zsh/aliases/general.zsh
@@ -4,9 +4,15 @@ alias h='history'
 
 alias grep='grep --color=auto'
 
+alias cd..='cd ..'
 alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
 
 alias shutdown='shutdown now'
 alias reboot='shutdown -r now'
+
+alias cp='cp -i'
+alias mv='mv -i'
+alias ln='ln -i'
+
